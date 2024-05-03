7cabda969e71bf4b766b6f0e9a5b243921c45d4f
hfdzz <yhafidz@gmail.com>
2024-05-03 15:41:05 +0700

    rename asset & update tanda terima

--- app/Views/template_surat/tanda_terima_yudisium.php
+++ app/Views/template_surat/tanda_terima_yudisium.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tanda Terima Yudisium <?= $nama ?></title>
+    <title>Surat Bebas UKT <?= $nama ?></title>
     <style>
         html{
             margin: 0;
@@ -11,21 +11,27 @@
         }
         
         body{
-            margin: 10mm 6mm;
+            margin: 0;
             padding: 0;
         }
+
+        #body{
+            /* margin: 10mm 20mm; */
+            padding: 0;
+        }
+
         .header {
             margin: 0;
             padding: 0;
             text-align: center;
         }
         main {
-            margin: 0 10mm;
+            margin: 0 3em;
             padding: 0;
             text-align: justify;
         }
         table {
-            margin: 0 10mm;
+            margin: 0;
             padding: 0;
         }
         /* indent for middle columnd */
@@ -36,42 +42,47 @@
     </style>
 </head>
 <body>
-    <!-- kop -->
-    <div class="header">
-        <img src="<?=$kop_surat?>" alt="kop" style="width: 100%;">
-        <p style="text-align: center;text-decoration:underline;font-weight:bold;">Tanda Terima Yudisium</p>
+    <div id="body">
+        <!-- kop -->
+        <div class="header">
+            <img src="<?= $kop_src ?>" alt="kop" style="width: 100%;">
+            <div style="text-align: center;text-decoration:underline;font-weight:bold;margin-top:1rem">TANDA TERIMA PENDAFTARAN YUDISIUM</div>
+        </div>
+        <main>
+            <br>
+            <p style="text-align: justify;">Telah Terima Berkas Pendaftaran Yudisium Periode Juni 2024 :</p>
+            <table>
+                <tr>
+                    <td>Nama</td>
+                    <td>:</td>
+                    <td><?= $nama ?></td>
+                </tr>
+                <tr>
+                    <td>NIM</td>
+                    <td>:</td>
+                    <td><?= $nim ?></td>
+                </tr>
+                <tr>
+                    <td>Program Studi</td>
+                    <td>:</td>
+                    <td><?= $program_studi ?></td>
+                </tr>
+            </table>
+    
+            <!-- <p style="text-align: justify;">Telah membayarkan UKT Sampai dengan Semester Akhir dan dinyatakan “LUNAS”.</p>
+            <p style="text-align: justify;">Demikian surat keterangan ini dikeluarkan untuk dipergunakan sebagaimana mestinya.</p> -->
+            <br>
+    
+            <div style="float: right;">
+                <div style="text-align: left;">Lampung Selatan, <?= $tanggal ?></div>
+                <div style="text-align: left;margin-top:0.5rem">Bendahara Penerimaan</div>
+                <br>
+                <br>
+                <br>
+                <div style="text-align: left;"><?= $peninjau->username ?></div> 
+                <div style="text-align: left;margin-top:0.5rem">NIP. <?= $peninjau->nip ?></div>
+            </div>
+        </main>
     </div>
-    <main>
-        <p style="text-align: justify;">Dengan ini kami menyatakan bahwa :</p>
-        <table>
-            <tr>
-                <td>Nama Mahasiswa</td>
-                <td>:</td>
-                <td><?= $nama ?></td>
-            </tr>
-            <tr>
-                <td>NIM</td>
-                <td>:</td>
-                <td><?= $nim ?></td>
-            </tr>
-            <tr>
-                <td>Program Studi</td>
-                <td>:</td>
-                <td><?= $program_studi ?></td>
-            </tr>
-            <tr>
-                <td>Fakultas</td>
-                <td>:</td>
-                <td>Fakultas Industry Technology Faculty</td>
-            </tr>
-        </table>
-        <p style="text-align: justify;">Mahasiswa ini telah diterima dalam Yudisium Fakultas Industry pada tanggal: <?= $tanggal ?>.</p>
-        <p style="text-align: right;">Dekan Fakultas Industry</p>
-        <br>
-        <br>
-        <br>
-        <p style="text-align: right;">Mr. Facultyhead, S.T., M.T</p>
-        <p style="text-align: right;">NIP. 123456789012</p>
-    </main>
 </body>
 </html>
